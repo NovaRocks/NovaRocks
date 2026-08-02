b6bff64b0231c985670645bf0f0697981e80d720
Harbor Liu <460660596@qq.com>
2026-08-02 09:28:15 +0800

    refactor(compat): remove StarRocks compat server

--- novarocks/compat/src/load/mod.rs
+++
@@ -1,9 +0,0 @@
-mod http;
-mod registry;
-mod service;
-mod tracking;
-
-pub(crate) use http::router;
-pub(crate) use registry::CompatLoadRegistry;
-pub(crate) use service::CompatLoadService;
-pub(crate) use tracking::LoadTrackingStore;
